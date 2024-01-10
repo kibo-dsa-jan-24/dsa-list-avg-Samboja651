bd7c4c211739760296f59176e2316b6fd8886af9
Samboja651 <waribekihiko651@gmail.com>
2024-01-10 11:11:13 +0300

    Learnt

--- test_listavg.py
+++ test_listavg.py
@@ -2,8 +2,22 @@ import unittest
 from listavg import ListAverage
 
 class TestListAverage(unittest.TestCase):
-    def test(self):
+    def test_compute_avg(self):
         lavg = ListAverage([1, 2, 3])
         assert lavg.compute_avg() == 2.0
 
+    def test_compute_avg_faster(self):
+        lavg = ListAverage([1, 2, 3])
+        assert lavg.compute_avg_faster() == 2.0    
+
+    def test_compute_avg_faster_add(self):
+        lavg = ListAverage([1, 2, 3])
+        lavg.add(10)
+        assert lavg.compute_avg_faster() == 4.0
+
+    def test_compute_avg_add(self):
+        lavg = ListAverage([1, 2, 3])
+        lavg.add(10)
+        assert lavg.compute_avg() == 4.0
+    
     # add more unit tests below
